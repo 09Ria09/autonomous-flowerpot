c4a46927629341ab02a63f8ff6970d1d11d6d8a2
Chiriac_Matei <chiriac.matei@gmail.com>
2021-01-18 20:46:46 +0200

    main

--- Flowerpot code/code/main.py
+++ Flowerpot code/code/main.py
@@ -1,2 +1,16 @@
-a = 4
-print(a)
+import Device
+
+device = Device()
+
+if __name__ == '__main__':
+    while True:
+        s = input()
+
+        if s == "add":
+            minWater, maxWater, waterPumpPort, waterSensorPort = input("minWater maxWater  waterPumpPort  waterSensorPort")
+            device.addPot(minWater, maxWater, waterPumpPort, waterSensorPort)
+
+        if s == "close":
+            break
+
+        device.runIteration()
