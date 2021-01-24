b0a1db95cfeb6bba82bab0584b0e73fb14ed6bb6
Chiriac Matei <chiriac.matei@gmail.com>
2021-01-24 18:25:30 +0000

    Update Flowerpot code/code/Device.py

--- Flowerpot code/code/Device.py
+++ Flowerpot code/code/Device.py
@@ -6,6 +6,9 @@ class Device:
 
     pots = []
 
+    def __init__(self):
+        gpio.setmode(gpio.BOARD)
+
     def add_pot(self, min_water, max_water, pump_pow, pump_in1, pump_in2):
         self.pots.append(Pot.Pot(min_water, max_water, pump_pow, pump_in1, pump_in2))
 
