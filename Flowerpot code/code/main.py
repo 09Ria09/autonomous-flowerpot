87344d1ca13d9515ec1492e956138b71f7b574da
Chiriac_Matei <chiriac.matei@gmail.com>
2021-01-26 01:19:51 +0200

    dasdas

--- Flowerpot code/code/main.py
+++ Flowerpot code/code/main.py
@@ -9,21 +9,11 @@ device.add_pot(0.4, 0.6, 7, 12, 11)
 # device.add_pot(0.4, 0.6, 18, 15, 16)
 
 while True:
-    s = ""
-    #s = input()
 
-    # if s == "add":
-
-    #   min_water = input("min_water")
-    #  max_water = input("max_water")
-
-    # pump ports
-    #    pump
-    #    device.add_pot(minWater, maxWater, waterPumpPort, waterSensorPort)
-
-    if s == "close":
+    try:
+        device.run_iteration()
+        print("called run_iteration")
+    except KeyboardInterrupt:
         gpio.cleanup()
         break
 
-    device.run_iteration()
-    print("called run_iteration")
