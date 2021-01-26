3f71ee4c2a9af258a237a1359c5a584c38bc2756
Chiriac_Matei <chiriac.matei@gmail.com>
2021-01-26 02:49:24 +0200

    dasdas

--- Flowerpot code/code/Device.py
+++ Flowerpot code/code/Device.py
@@ -126,7 +126,7 @@ class Device:
         self.last_page = -2
         if self.page == -1:
             self.page = self.row_selected - 2
-            self.row_selected = 2
+            self.row_selected = 1
 
         else:
             if self.row_selected == 4:
